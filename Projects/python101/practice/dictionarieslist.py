17023a28873f2d0680e5274b9df3212b8792bfc6
Zaria McBurrows <zaria@Zarias-MBP.attlocal.net>
2021-11-19 16:22:17 -0500

    first commit

---
+++ Projects/python101/practice/dictionarieslist.py
@@ -0,0 +1,16 @@
+username = input("Enter username: ")
+password = input("Enter password: ")
+
+home_page = {
+    "orders": {
+        "make_order": {
+            "new_options": "new camera",
+            "make_order_from_previous": ["measuring tape", "light sticks", "first aid kits",
+            "batteries"],
+        "continue_order": {
+            "shopping_cart": ["B&W camera", "twine", "bicycle with cart"]         
+        }
+            }
+        }
+}
+        
--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # git101
+# RPGproject.io
